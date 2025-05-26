dae8a559ce998a73163b504ff1be21744f0c0d2f
Max Barrett <maximilliangroup@gmail.com>
2025-05-25 19:25:44 -0700

    Create prettierrc.js
    Signed-off-by: Max Barrett <maximilliangroup@gmail.com>

---
+++ prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    ...require('@wordpress/prettier-config'), // If using WordPress's Prettier config
+    // Or define your own:
+    // semi: true,
+    // singleQuote: true,
+    // tabWidth: 2,
+    // trailingComma: 'es5',
+    // printWidth: 100,
+    // phpVersion: '7.4', // For prettier-plugin-php
+    // plugins: [require('prettier-plugin-php')], // If you want Prettier to format PHP
+    // overrides: [
+    //   {
+    //     files: '*.php',
+    //     options: {
+    //       tabWidth: 4, // WordPress PHP standard
+    //     },
+    //   },
+    // ],
+};
